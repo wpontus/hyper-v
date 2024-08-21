6d00dbb9661671624f1b278397ec355ee989dd0d
wpontus@github.com <wendtpontus@gmail.com>
2024-08-21 15:10:54 +0200

    Fixed som errors

--- new-vm/new-vm.ps1
+++ new-vm/new-vm.ps1
@@ -7,7 +7,7 @@
 
     Author : Pontus Wendt
     Twitter: @pontuswendt
-    Blog   : https://pontuswendt.blog
+    Blog   : https://clientstuff.blog
 
     Disclaimer: This script is provided "AS IS" with no warranties, confers no rights and 
     is not supported by the author or DeploymentArtist..
